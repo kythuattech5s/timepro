5c01b56f78d71c5a9a28834f6903df4756fd387f
= <=>
2022-09-18 13:59:38 +0700

    update code tien

--- public/theme/frontend/js/tabs.js
+++ public/theme/frontend/js/tabs.js
@@ -16,19 +16,20 @@ tabLinks.forEach(function (el) {
 
 
 function openTabs(el) {
-    var btn = el.currentTarget; // lắng nghe sự kiện và hiển thị các element
-    var electronic = btn.dataset.electronic; // lấy giá trị trong data-electronic
-
-    tabContent.forEach(function (el) {
-        el.classList.remove("active");
-    });
-
-    tabLinks.forEach(function (el) {
-        el.classList.remove("active");
-    });
+    var btn = el.currentTarget; // láº¯ng nghe sá»± kiá»‡n vĂ  hiá»ƒn thá»‹ cĂ¡c element
+    
+    var electronic = btn.dataset.electronic; // láº¥y giĂ¡ trá»‹ trong data-electronic
+    var data=btn.dataset.target;
+    var tab=Tech.$('[data-target]');
+    for(let i=0;i<tab._element.length - 1;i++){
+        if(tab._element[i]._element.dataset.target===data){
+            tab._element[i]._element.classList.remove("active");
+        }
+    }
 
     document.querySelector("#" + electronic).classList.add("active");
 
     btn.classList.add("active");
+    
 }
 /* end tab home page */
